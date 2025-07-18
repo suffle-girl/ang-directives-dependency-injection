61b72be6bbf9f59390b802f5a35b80d073a10660
Šárka Flídrová <sarka.flidrova@email.cz>
2025-07-18 16:23:26 +0200

    add updateTaskStatus to the service, use it on the taskItem, and add switch to computed tasks based on their status

--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from "@angular/core";
+import { Component, computed, inject, signal } from "@angular/core";
 
 import { TaskItemComponent } from "./task-item/task-item.component";
 import { TasksService } from "../tasks.service";
@@ -13,7 +13,24 @@ import { TasksService } from "../tasks.service";
 export class TasksListComponent {
   private tasksService = inject(TasksService);
   selectedFilter = signal<string>("all");
-  tasks = this.tasksService.allTasks;
+  tasks = computed(() => {
+    switch (this.selectedFilter()) {
+      case "open":
+        return this.tasksService
+          .allTasks()
+          .filter((task) => task.status === "OPEN");
+      case "in-progress":
+        return this.tasksService
+          .allTasks()
+          .filter((task) => task.status === "IN_PROGRESS");
+      case "done":
+        return this.tasksService
+          .allTasks()
+          .filter((task) => task.status === "DONE");
+      default:
+        return this.tasksService.allTasks();
+    }
+  });
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
