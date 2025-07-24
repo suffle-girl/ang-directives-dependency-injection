1d8c02184208280af8f97d98408fd6fa76781416
Šárka Flídrová <sarka.flidrova@email.cz>
2025-07-24 17:09:24 +0200

    custom dependency injection tokens and providers

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 
 import { AppComponent } from "./app/app.component";
-// import { TasksService } from "./app/tasks/tasks.service";
+import { TasksService } from "./app/tasks/tasks.service";
+import { InjectionToken } from "@angular/core";
 
-// bootstrapApplication(AppComponent, {
-//   providers: [TasksService],
-// }).catch((err) => console.error(err));
+export const TasksServiceToken = new InjectionToken<TasksService>(
+  "tasks-service-token"
+);
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [{ provide: TasksServiceToken, useClass: TasksService }],
+}).catch((err) => console.error(err));
+
+// bootstrapApplication(AppComponent).catch((err) => console.error(err));
