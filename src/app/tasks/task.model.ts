11441ad32392a499bbd9c4ff261475925cef6410
Šárka Flídrová <sarka.flidrova@email.cz>
2025-07-25 09:22:25 +0200

    prepare TasksStatusOptions for the injection as non-service value

--- src/app/tasks/task.model.ts
+++ src/app/tasks/task.model.ts
@@ -1,4 +1,26 @@
-export type TaskStatus = 'OPEN' | 'IN_PROGRESS' | 'DONE';
+export type TaskStatus = "OPEN" | "IN_PROGRESS" | "DONE";
+
+export const TaskStatusOptions: {
+  value: "open" | "in-progress" | "done";
+  taskStatus: TaskStatus;
+  text: string;
+}[] = [
+  {
+    value: "open",
+    taskStatus: "OPEN",
+    text: "Open",
+  },
+  {
+    value: "in-progress",
+    taskStatus: "IN_PROGRESS",
+    text: "In-Progress",
+  },
+  {
+    value: "done",
+    taskStatus: "DONE",
+    text: "Completed",
+  },
+];
 
 export interface Task {
   id: string;
